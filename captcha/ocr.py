1a413ea9f3fde0ac1fd015ff96917de8b9939c28
migerh <michael.gerhaeuser@gmail.com>
2010-09-03 13:55:46 +0200

    ocr with tesseract via ajax

--- captcha/ocr.py
+++ captcha/ocr.py
@@ -1,29 +1,35 @@
+#!/usr/bin/env python
 import base64
 import cgi
+import uuid
+import subprocess
+import os
 
 form = cgi.FieldStorage();
-data = base64.b64decode(form.getfirst('data', ''))
-png = '/tmp/'+str(uuid.uuid4()) + '.png'
-pbm = '/tmp/'+str(uuid.uuid4()) + '.pbm'
-tif = '/tmp/'+str(uuid.uuid4()) + '.tif'
-txt = '/tmp/'+str(uuid.uuid4()) + '.txt'
+data = base64.b64decode(form.getfirst('data', '').replace(" ", "+"))
+
+png = '/tmp/'+str(uuid.uuid4()).replace('-', '') + '.png'
+pbm = '/tmp/'+str(uuid.uuid4()).replace('-', '') + '.pbm'
+tif = '/tmp/'+str(uuid.uuid4()).replace('-', '') + '.tif'
+txt = '/tmp/'+str(uuid.uuid4()).replace('-', '')
 
 f = open(png, "w")
 f.write(data)
+f.close()
 
-pngtopbm_process = subprocess.Popen(["convert", png, pbm], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
-pngtopbm.communicate('')[0]
-pbmtotif_process = subprocess.Popen(["convert", pbm, tif], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
-pbmtotif.communicate('')[0]
-tiftotxt_process = subprocess.Popen(["tesseract", tif, txt], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
-tiftotxt.communicate('')[0]
-cattxt_process = subprocess.Popen(["cat", txt], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
-code = cattxt.communicate('')[0]
+os.system("convert " + png + " " + pbm)
+os.system("convert " + pbm + " " + tif)
+
+os.system("tesseract " + tif + " " + txt)
+
+f = open(txt + ".txt", "r")
+code = f.read()
+
 
 os.remove(png)
 os.remove(pbm)
 os.remove(tif)
-os.remove(txt)
+os.remove(txt + ".txt")
 
 print """\
 Content-Type: text/plain\n
